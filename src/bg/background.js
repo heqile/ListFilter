9f7a37c26d1bd5bee9666906089cb858de5f9a4e
qile <mr.qile@gmail.com>
2022-01-25 00:59:51 +0100

    feat: use event listener

---
+++ src/bg/background.js
@@ -0,0 +1,14 @@
+
+chrome.action.onClicked.addListener(function(tab) {
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, {handler: "listFilter"}, function(response) {
+        });
+      });
+});
+
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+    if (changeInfo.status === "complete") {
+        chrome.tabs.sendMessage(tabId, {handler: "listFilter"}, function(response) {
+        });
+    }
+});
--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,4 +1,4 @@
-chrome.extension.sendMessage({}, function(response) {
+chrome.runtime.sendMessage({}, function(response) {
     var readyStateCheckInterval = setInterval(function() {
         if (document.readyState === "complete") {
             clearInterval(readyStateCheckInterval);
@@ -8,23 +8,32 @@ chrome.extension.sendMessage({}, function(response) {
 });
 
 function domain_control() {
-	var store = chrome.storage.local;
-	store.get("list", function (result) {
-		list = result.list;
-		if (list && typeof(list) !== "object") {
-			store.remove("list");
-			console.log("Oops, something is wrong with the data!");
-			return;
-		}
-		else if (!list) {
-			return;
-		}
-		for (i = 0; i < list.length; i++) {
-			var regex = new RegExp(list[i].domain);
-			if (document.URL.match(regex)) {
-				$(document).ready(select_dispatcher);
-				return;
-			}
-		}
-	});
+    var store = chrome.storage.local;
+    store.get("list", function (result) {
+        list = result.list;
+        if (list && typeof(list) !== "object") {
+            store.remove("list");
+            console.log("Oops, something is wrong with the data!");
+            return;
+        }
+        else if (!list) {
+            return;
+        }
+        for (i = 0; i < list.length; i++) {
+            var regex = new RegExp(list[i].domain);
+            if (document.URL.match(regex)) {
+                $(document).ready(select_dispatcher);
+                return;
+            }
+        }
+    });
 }
+
+chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+      if (request.handler === "listFilter")
+          $(document).ready(function() {
+			select_dispatcher(); 
+        });
+    }
+  );
