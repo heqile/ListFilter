37fc7962c182ef7111b25ed9541486652c589594
qile <mr.qile@gmail.com>
2022-01-26 00:56:25 +0100

    feat: remove option page and config file

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -16,33 +16,11 @@ function select_dispatcher() {
     });
 }
 
-function domain_control() {
-    var store = chrome.storage.local;
-    store.get("list", function (result) {
-        list = result.list;
-        if (list && typeof(list) !== "object") {
-            store.remove("list");
-            console.log("Oops, something is wrong with the data!");
-            return;
-        }
-        else if (!list) {
-            return;
-        }
-        for (i = 0; i < list.length; i++) {
-            var regex = new RegExp(list[i].domain);
-            if (document.URL.match(regex)) {
-                $(document).ready(select_dispatcher);
-                return;
-            }
-        }
-    });
-}
-
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
       if (request.handler === "listFilter")
           $(document).ready(function() {
-            select_dispatcher(); 
+            select_dispatcher();
         });
     }
 );
